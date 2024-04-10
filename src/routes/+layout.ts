8dcf0feeddfd96c21d4e8082bd1711b1bdd2eee5
Fernando Merino <fernandomema@gmail.com>
2024-04-10 07:05:32 +0000

    Add i18n

---
+++ src/routes/+layout.ts
@@ -0,0 +1,11 @@
+import { loadTranslations } from '$lib/translations';
+
+export const load = async ({ url }: any) => {
+    const { pathname } = url;
+
+    const initLocale = 'en'; // get from cookie, user session, ...
+
+    await loadTranslations(initLocale, pathname); // keep this just before the `return`
+
+    return {};
+}
